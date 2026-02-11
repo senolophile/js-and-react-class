2ba0828cb3c4df0672e5f0e334192d4f1a8afa2e
senelophile <partimadhakal2006@gmail.com>
2026-02-11 09:00:09 +0545

    fix

---
+++ hello.js
@@ -0,0 +1,42 @@
+// //operators in javascript
+// // arthimetic operator
+// //comparison operators
+// //USED TO COMPARE TWO OR MORE VALUES AND THE RESULT T RETURNED JS ALWAYS BOOLEAN VALUE THAT JS EITHER TRUE OR FALSE
+
+// let a=1
+// let b=8
+// //console.log(a>b)
+// console.log(a<b)
+// //greater than equals to
+// //console.log(a>=b)
+
+// //== ==
+// // == checks only value but ignores the data types
+// //== trict equality checker both value as well as data types
+// let a=10
+// let b="10"
+// //console.log(a==b)
+// console.log(a==b)
+
+
+
+// //write a program to convert the degree  celsius  to  degree fahrenheit [celsius*(9/5)+32]
+// const celsius=36
+// const fahrenheit=(celsius*(9/5)+32)
+// console.log("The "+celsius+"degree"+" is equal to "+fahrenheit+"fahreinheit")
+
+
+
+
+
+
+// let c=20
+
+// c=c+1
+// c+=1
+// // console.log("first:",c)
+// c+=1
+// //console.log("second:",c)
+
+
+
